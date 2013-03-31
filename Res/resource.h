a957cd96a7b107ae17b8d4bb4c499411151b05b3
Mihai Popescu <mr.mihaipopescu@gmail.com>
2013-03-31 22:43:15 +0300

    * VS2010 Express

--- Res/resource.h
+++ Res/resource.h
@@ -1,16 +1,5 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Game.rc
-//
-#define IDI_ICON                        102
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40015
-#define _APS_NEXT_CONTROL_VALUE         1007
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+#define IDI_ICON                                102
