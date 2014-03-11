0261b41a1ce4fcf833d0a2b0c67b3b6bb0352211
Mihai Popescu <mr.mihaipopescu@gmail.com>
2014-03-11 22:08:12 +0200

    * switched to premake

---
+++ _Solution/premake5.lua
@@ -0,0 +1,52 @@
+solution "GameFramework"
+	configurations { "Debug", "Release" }
+
+		location ".."
+	
+		project "Game"
+		kind "WindowedApp"
+		language "C++"
+		
+		
+		objdir "Compiled"
+		debugdir "."
+		
+		files {
+			"../Includes/*.h",
+			"../Source/*.cpp",
+			"../Docs/*.txt",
+		}
+		
+		includedirs {
+			"../Includes",
+		}
+		
+		links {
+			"winmm.lib",
+			"Msimg32.lib",
+		}
+		
+		flags {
+			"WinMain",
+			"FatalWarnings"
+		}
+
+		configuration { "windows", "vs*" }
+			defines { "WIN32" }
+			flags { "NoIncrementalLink", "NoEditAndContinue", "NoPCH" }
+			
+			files {
+				"../Res/*.rc",
+				"../Res/*.h",
+			}
+
+			configuration "Debug"
+				defines { "_DEBUG" }
+				flags { "Symbols" }
+				targetprefix "d_"
+
+			configuration "Release"
+				defines { "NDEBUG" }
+				optimize "Speed"
+			
+
